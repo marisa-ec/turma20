16b5edbc6c1c756df914cacb7858e1ecff269d8b
Eleoteriop <patrick.eleoterio@hotmail.com>
2022-06-22 20:47:15 -0300

    Função raiz quadrada

--- soma.py
+++ soma.py
@@ -7,3 +7,13 @@ num1 = int(input('Valor do primeiro número: '))
 num2 = int(input('Valor do segundo número: '))
 
 print(somaDoisValores(num1, num2))
+
+def raizValores(a,b):
+	return a ** b
+
+print("Olá pessoal, vamos elevar?")
+
+num1 = int(input('Valor do primeiro número: '))
+num2 = int(input('Valor do segundo número: '))
+
+print(raizValores(num1, num2))
