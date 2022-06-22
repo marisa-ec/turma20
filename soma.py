cef811a2c0418e69da6444111d49fc50bc1d45bb
Matheus Barbosa Almeida <105905645+MatheusB2002@users.noreply.github.com>
2022-06-22 20:49:33 -0300

    Update soma.py

--- soma.py
+++ soma.py
@@ -1,9 +1,10 @@
-def somaDoisValores(a,b):
-	return a + b
+def somaDoisValores(a,b,c):
+	return a + b + c
 
 print("Olá pessoal, vamos somar?")
 
 num1 = int(input('Valor do primeiro número: '))
 num2 = int(input('Valor do segundo número: '))
+num3 = int(input('Valor do terceiro número: '))
 
-print(somaDoisValores(num1, num2))
+print(somaDoisValores(num1, num2, num3))
