028178a7e8da54f63f2e23d99635790191a1d490
PedroCampos <programapedror@gmail.com>
2022-06-22 20:23:17 -0300

    def divisao

--- soma.py
+++ soma.py
@@ -1,9 +1,28 @@
 def somaDoisValores(a,b):
 	return a + b
 
-print("Olá pessoal, vamos somar?")
+def divisaoDoisValores(a,b):
+	return a/b
 
+
+dec = int(input('1. Somar\n2. Dividir\n>'))
 num1 = int(input('Valor do primeiro número: '))
 num2 = int(input('Valor do segundo número: '))
+
+
+if dec == 1:
+	print("Vamos somar!")
+	somaDoisValores(num1,num2)
+elif dec == 2:
+	print('Vamos dividir.')
+	divisaoDoisValores(num1,num2)
+else:
+	while dec != 1 or dec != 2:
+		print('digite uma opção valida')
+		dec = int(input(''))
+
+		
+
+
 
 print(somaDoisValores(num1, num2))
