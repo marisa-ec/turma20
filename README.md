fc1cd65afb0bbcf680625b2caf45b035d774ffce
Marisa Silva <marisa.silva@datamint.com.br>
2022-06-22 18:33:49 -0300

    função adição

---
+++ README.md
@@ -0,0 +1,8 @@
+# Projeto - Módulo 1
+
+### Turma 20
+
+
+![resilia](https://www.resilia.com.br/wp-content/uploads/2021/08/logo.png)
+
+
---
+++ soma.py
@@ -0,0 +1,9 @@
+def somaDoisValores(a,b):
+	return a + b
+
+print("Olá pessoal, vamos somar?")
+
+num1 = int(input('Valor do primeiro número: '))
+num2 = int(input('Valor do segundo número: '))
+
+print(somaDoisValores(num1, num2))
